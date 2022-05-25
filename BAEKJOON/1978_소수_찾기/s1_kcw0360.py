38c1cddfd268a7f34c552c5924ecbefdd028ef05
kcw0360 <kcw0360@naver.com>
2022-05-25 15:22:43 +0900

    1978 소수 찾기

---
+++ BAEKJOON/1978_소수_찾기/s1_kcw0360.py
@@ -0,0 +1,18 @@
+N = int(input())
+
+numbers = list(map(int, input().split()))
+
+cnt = 0    # 소수 개수 카운팅
+for i in numbers:
+    result = []    # 약수 저장
+
+    # 약수 찾기
+    for j in range(1, i+1):
+        if i % j == 0:
+            result.append(i)
+
+    # 약수가 두개면 소수 (1, input)라 판단 하여 카운팅 하기
+    if len(result) == 2:
+        cnt += 1
+
+print(cnt)
