bd71c5e5afb203cd2e6bbf25f7465fa75ffe36f4
KimSoonYo <rlatnsdy125@naver.com>
2022-06-10 03:11:41 +0900

    124나라의 숫자 주석 추가

--- programmers/Lv2_124나라의숫자/s1_KSoonYo.py
+++ programmers/Lv2_124나라의숫자/s1_KSoonYo.py
@@ -16,3 +16,14 @@ def solution(n):
     answer = temp.replace('3', '4')
     return answer
 
+## 상백님 풀이
+'''
+def solution(n):
+    n -= 1
+    if n < 3:
+        return '124'[n]
+    else:
+        q, r = divmod(n, 3)
+        return solution(q) + '124'[r]
+
+'''
