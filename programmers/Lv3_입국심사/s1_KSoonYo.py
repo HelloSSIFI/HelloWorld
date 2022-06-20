70722184d5ed03758c6aa976f60816af0054d938
KimSoonYo <rlatnsdy125@naver.com>
2022-06-21 00:19:17 +0900

    KSoonYo programmers Lv3_입국심사 풀이 수정

--- programmers/Lv3_입국심사/s1_KSoonYo.py
+++ programmers/Lv3_입국심사/s1_KSoonYo.py
@@ -1,29 +1,58 @@
 
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
 def solution(n, times):
     '''
     n: 심사받아야 하는 사람 수
     times: 심사관들의 심사 시간
     '''
     answer = 0
-    time_table = [i for i in range(max(times) * n + 1)]
-    
+
     left = 0
-    right = len(time_table) - 1
-    while left < right:
+    right = max(times) * max(times)
+    answer = right
+    while left <= right:
         people = 0
         mid = (left + right) // 2
 
         for time in times:
             people += (mid // time)
-        
+            if people >= n:
+                break
+
         if people >= n:
             answer = mid        
             right = mid - 1
-        
-        else:
+
+        elif people < n:
             left = mid + 1
 
 
     return answer
-
 print(solution(6, [7, 10]))
