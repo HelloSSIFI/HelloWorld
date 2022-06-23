4eb4014520eeea055d466d1a7a7606f8422883bc
kcw0360 <kcw0360@naver.com>
2022-06-23 11:47:49 +0900

    1038 감소하는 수

---
+++ BAEKJOON/1038_감소하는_수/s1_kcw0360.py
@@ -0,0 +1,19 @@
+from itertools import combinations
+
+N = int(input())
+
+numbers = []
+
+if N > 1022:    # 9876543210이 N이 1022일 때 이므로 이 이상 큰 수는 감소하는 수 불가능
+    print(-1)
+else:
+    for length in range(1, 11):    # 자릿수 1~10 경우를 찾기 위함
+        for temp in combinations(range(10), length):    # 중복되지 않은 수의 조합을 모두 찾는다.
+            number = list(temp)
+            number.sort(reverse=True)    # 내림차순
+            num = ''.join(map(str, number))    # 하나의 숫자로 변환
+            numbers.append(int(num))    # 숫자로 바꿔 리스트에 추가
+
+    numbers.sort()    # 오름차순으로 정렬, N = idx
+
+    print(numbers[N])
