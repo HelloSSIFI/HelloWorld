d3312a8f79d48fc34fef3be102291fa64d289feb
hong <hyg4779@naver.com>
2022-06-24 16:52:40 +0900

    0625 스터디 문제풀이

--- BAEKJOON/1038_감소하는_수/s1_hyg4779.py
+++ BAEKJOON/1038_감소하는_수/s1_hyg4779.py
@@ -1,36 +1,32 @@
 N = int(input())
 if N < 10:
     print(N)
+    exit()
 num = 10
 cnt = 9
 
 
-def assemble(top, n, word):
+def assemble(n, word):
     global cnt
 
     if n == M:      # 해당 숫자의 길이가 다 되면 감소하는 수인지 검사
-        for i in range(1, n):
-            if int(word[i-1]) <= int(word[i]):break
-        else:
-            cnt += 1
-            if cnt == N:
-                print(word)
-                exit()
-
+        cnt += 1
+        if cnt == N:
+            print(word)
+            exit()
         return
 
-    for i in range(top):
+    for i in range(int(word[-1])):
         new = word + str(i)
-        assemble(i, n+1, new)
-
-
+        assemble(n+1, new)
 
 
 while num <= 1000000:
     M = len(str(num))       # 숫자의 길이
     top = str(num)[0]       # 가장 큰 수
 
-    assemble(int(top), 1, str(top))
+    assemble(1, str(top))
     num += 10**(M-1)
 else:
     print(-1)
+
