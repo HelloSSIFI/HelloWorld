2e181b2db337f279d5c82a1a1cbc054f02548e17
kcw0360 <kcw0360@naver.com>
2022-06-27 10:58:01 +0900

    Lv2 K진수에서 소수 개수 구하기

---
+++ programmers/Lv2_k진수에서_소수_개수_구하기/s1_kcw0360.py
@@ -0,0 +1,34 @@
+def solution(n, k):
+    answer = 0
+    res = ''
+    m = 12
+
+    while n > 0:
+        num = k**m
+        if num > n:
+            res += str(0)
+        else:
+            a, b = divmod(n, num)
+            res += str(a)
+            n = b
+
+        m -= 1
+
+    temp = res.split('0')
+
+    for i in temp:
+        if i == '' or int(i) == 1:
+            continue
+
+        check = True
+        for j in range(2, int(int(i)**0.5) + 1):
+            if int(i) % j == 0:
+                check = False
+                break
+
+        if check:
+            answer += 1
+
+    return answer
+
+print(solution(437674, 3))
