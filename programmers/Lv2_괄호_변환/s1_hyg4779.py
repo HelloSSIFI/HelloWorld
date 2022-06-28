2726b621c3f117b8f10cfcf5f543cdc532384068
hong <hyg4779@naver.com>
2022-06-28 20:56:53 +0900

    0628 스터디 문제풀이

--- programmers/Lv2_괄호_변환/s1_hyg4779.py
+++ programmers/Lv2_괄호_변환/s1_hyg4779.py
@@ -4,28 +4,28 @@ from collections import deque
 def solution(p):
     if not p:return p               # 빈 문자열이면 그대로 return
 
-    def check(word):
+    def check(word):                # 검사하는 함수
         queue = deque([])
 
-        for w in word:
+        for w in word:              # 열린 괄호 무조건 append
             if w == '(':
                 queue.append(w)
-            else:
-                if not queue:
+            else:                   # 닫힌 괄호일 때
+                if not queue:       # 열린 괄호 없으면 잘못된 괄호열
                     return False
-                queue.pop()
+                queue.pop()         # 열린 괄호 pop
 
         return True
 
 
-    def divide(word):
+    def divide(word):                               # u, v 나누는 함수
         l, r, = 0, 0
-        for i in range(len(word)):
+        for i in range(len(word)):                  # 열린 괄호 닫힌 괄호 숫자 세기
             if word[i] == '(':
                 l += 1
             else:
                 r += 1
-            if l == r:
+            if l == r:                              # 서로 같은 수일 때 나눠서 return
                 return word[:i+1], word[i+1:]
 
     u, v = divide(p)
