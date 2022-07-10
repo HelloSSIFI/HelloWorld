c62fe78590885c02ff7b9f9da58076915a0e7c92
hong <hyg4779@naver.com>
2022-07-11 01:44:35 +0900

    0712 스터디 문제풀이

--- programmers/Lv2_3차_방금그곡/s1_hyg4779.py
+++ programmers/Lv2_3차_방금그곡/s1_hyg4779.py
@@ -1,4 +1,36 @@
 def solution(m, musicinfos):
-    answer = ''
-    return answer
+    answer = None
+    # 시작, 끝, 곡이름, 멜로디
+    m = m.replace("C#", "c").replace("D#", "d").replace("F#", "f").replace("G#", "g").replace("A#", "a")
 
+    for now in musicinfos:
+        s, e, name, tmp = now.split(',')
+        tmp = tmp.replace("C#", "c").replace("D#", "d").replace("F#", "f").replace("G#", "g").replace("A#", "a")
+
+        sh, sm = s.split(':')
+        eh, em = e.split(':')
+
+        s = int(sh)*60+int(sm)
+        e = int(eh)*60+int(em)
+
+        time = e-s
+
+
+        tmp *= time//len(tmp)+1
+        tmp = tmp[:time]
+
+
+        if m in tmp:
+            if answer == None or answer[1] < time or (answer[1] == time and answer[2] > s):
+                answer = (name, time, s)
+
+
+    if answer:
+        return answer[0]
+
+    return "(None)"
+
+
+print(solution("ABCDEFG", ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
+print(solution("CC#BCC#BCC#BCC#B", ["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]))
+print(solution("ABC", ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
