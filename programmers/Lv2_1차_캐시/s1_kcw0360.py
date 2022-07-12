84f4390527f2eeb3993c213920939919f487011e
kcw0360 <kcw0360@naver.com>
2022-07-12 10:23:34 +0900

    주석 및 코드 변경

--- programmers/Lv2_1차_캐시/s1_kcw0360.py
+++ programmers/Lv2_1차_캐시/s1_kcw0360.py
@@ -2,23 +2,23 @@ from collections import deque
 
 def solution(cacheSize, cities):
     answer = 0
-    memory = deque()
+    db = deque()
 
-    if cacheSize == 0:
+    if cacheSize == 0:    # 캐시 크기가 0이면 저장 불가능
         return len(cities) * 5
     else:
         for city in cities:
-            city = city.lower()
+            city = city.lower()    # 대소문자 구분없게 하기 위해 소문자로 통일
 
-            if city in memory:
-                answer += 1
-                memory.remove(city)
-                memory.append(city)
-            else:
-                if len(memory) == cacheSize:
-                    memory.popleft()
-                answer += 5
-                memory.append(city)
+            if city in db:    # DB에 존재하는 도시일 경우
+                answer += 1    # hit일 경우 실행시간 추가
+                db.remove(city)    # 캐시 교체 알고리즘(LRU) 적용
+                db.append(city)    # 해당 도시를 제거 후 재기록
+            else:    # DB에 존재하지 않는 도시일 경우
+                if len(db) == cacheSize:    # DB 캐시의 크기가 최대인 경우
+                    db.popleft()    # 가장 오래된 것을 삭제
+                answer += 5    # miss일 경우 실행시간 추가
+                db.append(city)    # DB캐시에 도시 이름 추가
 
     return answer
 
--- programmers/Lv2_3차_방금그곡/s1_kcw0360.py
+++ programmers/Lv2_3차_방금그곡/s1_kcw0360.py
@@ -1,10 +1,12 @@
 def check(melody, info):
     res = []
 
+    # 시작, 끝 시간의 차를 분으로 환산하여 재생시간 구하기
     hours = (int(info[1][0:2]) - int(info[0][0:2])) * 60
     minutes = int(info[1][3:5]) - int(info[0][3:5])
     time_diff = hours + minutes
 
+    # 멜로디와 같이 곡의 음 정보를 리스트로 만들어 주기
     pitch = []
     tmp = ''
     for i in range(len(info[3])-1, -1, -1):
@@ -18,30 +20,34 @@ def check(melody, info):
         else:
             pitch = [info[3][i]+tmp] + pitch
 
+    # 곡 재생시간을 통해 몇번 반복했으며 어느 구간까지 재생되었는지 확인 후 만들어 주기
     a, b = divmod(time_diff, len(pitch))
     pitch = pitch * a + pitch[:b]
 
-    length = len(pitch) - len(melody)
+    length = len(pitch) - len(melody)    # 기억하고 있는 부분과 재생된 곡의 음정보 길이 차이
 
-    if length > 0:
-        for i in range(len(pitch)):
+    if length > 0:    # 재생된 것의 음의 길이가 더 긴 경우
+        for i in range(len(pitch)):    # 기억한 멜로디와 같은 길이로 자른 후 비교
             if pitch[i:i+len(melody)] == melody:
-                res.append(time_diff)
+                res.append(time_diff)    # 같다면 결과 값에 시간, 곡 이름 저장
                 res.append(info[2])
-                break
-    elif length == 0:
+                break    # 찾았기 때문에 반복문 빠져 나오기
+    elif length == 0:    # 길이가 같은 경우 일치 여부 확인 후 같다면 곡 재생시간, 곡 이름 저장
         if pitch == melody:
             res.append(time_diff)
             res.append(info[2])
-    if time_diff == 0:
+
+    if time_diff == 0:    # 재생이 안된 경우
         return []
+
     return res
 
 
 def solution(m, musicinfos):
     result = []
 
-    melody = []
+    # 멜로디를 리스트로 만들기 (#과 문자를 하나의 요소로 저장하기 위함)
+    melody = []    # 기억한 멜로디 저장
     tmp = ''
     for i in range(len(m) - 1, -1, -1):
         if m[i] == '#':
@@ -54,11 +60,15 @@ def solution(m, musicinfos):
         else:
             melody = [m[i] + tmp] + melody
 
+    # 곡 정보 순회하면서 멜로디와 비교
     for i in musicinfos:
         temp = check(melody, i.split(','))
-        if temp:
+        if temp:    # temp가  빈 리스트가 아닐 경우 결과 값에 저장
             result.append(temp)
 
+    # 빈 리스트면 (None) 출력
+    # 존재 한다면 result의 곡 정보를 순회하면서 temp에 재생시간, answer에 곡명을 담으며 체크
+    # 가장 긴 재생시간이며, 동시간 재생일 경우 먼저 언급된 곡을 결과 값으로 출력
     if result:
         temp = 0
         answer = ''
