b974b595ef71a7314f05911cb645a398e3e413dd
hong <hyg4779@naver.com>
2022-07-19 21:18:57 +0900

    0719 스터디 문제풀이

--- programmers/Lv2_프린터/s1_hyg4779.py
+++ programmers/Lv2_프린터/s1_hyg4779.py
@@ -1,23 +1,20 @@
 def solution(priorities, location):
-    N = len(priorities)
     answer = 0
     priorities[location] *= -1
 
     while priorities:
         now = priorities.pop(0)
-        N -= 1
         flag = False
 
         if now < 0:
             now *= -1
             flag = True
 
-        for i in range(N):
-            if abs(priorities[i]) > now:
+        for tmp in priorities:
+            if abs(tmp) > now:
                 if flag:
                     now *= -1
                 priorities.append(now)
-                N += 1
                 break
         else:
             answer += 1
