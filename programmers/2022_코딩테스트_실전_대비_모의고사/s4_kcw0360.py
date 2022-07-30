4a14e9dd2f2a7ba2f76bfe880856377211038298
kcw0360 <kcw0360@naver.com>
2022-07-30 15:04:53 +0900

    2022 코딩테스트 실전 대비 모의고사

---
+++ programmers/2022_코딩테스트_실전_대비_모의고사/s4_kcw0360.py
@@ -0,0 +1,13 @@
+def solution(beginning, target):
+    answer = 0
+    n = len(beginning)    # 세로
+    m = len(beginning[0])    # 가로
+
+    temp = 0
+    for i in range(n):
+        for j in range(m):
+
+    return answer
+
+
+print(solution([[0, 1, 0, 0, 0], [1, 0, 1, 0, 1], [0, 1, 1, 1, 0], [1, 0, 1, 1, 0], [0, 1, 0, 1, 0]], [[0, 0, 0, 1, 1], [0, 0, 0, 0, 1], [0, 0, 1, 0, 1], [0, 0, 0, 1, 0], [0, 0, 0, 0, 1]]))
