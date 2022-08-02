96da33bcc2ba6ae60d2631c6bad9ddbecd8f4b5b
hong <hyg4779@naver.com>
2022-08-02 15:00:21 +0900

    0802 스터디 문제풀이

--- programmers/Lv3_순위/s1_hyg4779.py
+++ programmers/Lv3_순위/s1_hyg4779.py
@@ -10,9 +10,10 @@ def solution(n, results):
 
     for me in range(1, n+1):
 
+        # me가 이긴 애들은 me가 진 사람한테도 진 것
         for idx in win[me]:
             lose[idx].update(lose[me])
-
+        # me를 이긴 애들은 me가 이긴 사람한테도 이긴 것
         for idx in lose[me]:
             win[idx].update(win[me])
 
