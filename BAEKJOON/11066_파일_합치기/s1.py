9a5017d5e7cf8f14aa3a296b4b092b39f539947b
hong <hyg4779@naver.com>
2022-08-26 15:51:24 +0900

    0827 스터디 문제풀이

--- BAEKJOON/11066_파일_합치기/s1.py
+++ BAEKJOON/11066_파일_합치기/s1.py
@@ -1,7 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-direct = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+for _ in range(int(input())):
+    n = int(input())
+    arr = [0]+list(map(int, input().split()))
 
-for tc in range(int(input())):
-    pass
+    # 누적합 구해주기
+    for i in range(1, n+1):
+        arr[i] += arr[i-1]
+
+    # dp[i][j]: i에서 j까지 합하는 데 최소 비용
+    dp = [[0 for _ in range(n+1)] for _ in range(n+1)]
+    for idx in range(2, n+1):
+        for j in range(1, n+2-idx):
+            dp[j][j+idx-1] = min([dp[j][j+k]+dp[j+k+1][j+idx-1] for k in range(idx-1)])+(arr[j+idx-1]-arr[j-1])
+
+    print(dp[1][n])
