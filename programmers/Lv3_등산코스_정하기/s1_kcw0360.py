44329bcccf308e6d5367f6a4d4a473c788cde573
kcw0360 <kcw0360@naver.com>
2022-09-05 14:12:22 +0900

    Lv3 등산코스 정하기

--- programmers/Lv3_등산코스_정하기/s1_kcw0360.py
+++ programmers/Lv3_등산코스_정하기/s1_kcw0360.py
@@ -1,20 +1,23 @@
 from collections import defaultdict
+import heapq
+
 
 def solution(n, paths, gates, summits):
-    answer = [0, 10000001]
+    answer = []
+    summits = set(summits)    # 중복 제거
     mt = defaultdict(list)    # 길 정보
-    for a, b, c in paths:
+    for a, b, c in paths:    # 양방향으로 길 정보 저장
         mt[a].append([b, c])
         mt[b].append((a, c))
 
     q = []
     visited = [10000001] * (n+1)    # 방문지 체크, 주어진 조건 최대값을 각 위치에 저장
     for gate in gates:
-        q.append([gate, 0])    # [gate, intensity]
+        heapq.heappush(q, [gate, 0])    # [gate, intensity]
         visited[gate] = 0    # 출입구 방문 체크
 
     while q:
-        now, intensity = q.pop()
+        now, intensity = heapq.heappop(q)
 
         # 산봉우리에 도착하거나 intensity가 더 큰 지점은 더 이상 이동하지 않는다.
         if (now in summits) or (intensity > visited[now]):
@@ -22,14 +25,14 @@ def solution(n, paths, gates, summits):
 
         for nxt, time in mt[now]:    # 현 위치에서 갈 수 있는 곳 찾기
             temp = max(intensity, time)    # 다음 위치와 현 위치 중 시간이 큰 것을 temp에 저장
-
             # temp보다 작은 경우는 지나온 길이기 때문에 체크하지 않는다.
             if temp < visited[nxt]:
                 visited[nxt] = temp
-                q.append([nxt, temp])
+                heapq.heappush(q, [nxt, temp])
 
     for summit in summits:
-        if visited[summit] < answer[1]:
-            answer = [summit, visited[summit]]
+        answer.append([summit, visited[summit]])
 
-    return answer
+    answer.sort(key=lambda x: (x[1], x[0]))    # 1순위 intensity, 2순위로 산봉우리 번호로 오름차순 정렬
+
+    return answer[0]
