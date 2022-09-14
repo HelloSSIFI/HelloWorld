15a0e21629c22f0256fb60dbddcbe74c4f2fa80a
kcw0360 <kcw0360@naver.com>
2022-09-14 10:09:13 +0900

    14425 문자열 집합(수정)

--- BAEKJOON/14425_문자열_집합/s1_kcw0360.py
+++ BAEKJOON/14425_문자열_집합/s1_kcw0360.py
@@ -8,13 +8,9 @@ n_s = []
 for _ in range(N):
     n_s.append(input())
 
-m_s = []
-for _ in range(M):
-    m_s.append(input())
-
 answer = 0
-for m in m_s:    # M개의 문자열을 차례대로 N개의 문자열에 포함되는지 확인
-    if m in n_s:
+for _ in range(M):    # M개의 문자열을 차례대로 N개의 문자열에 포함되는지 확인
+    if input() in n_s:
         answer += 1
 
 print(answer)
