41d7ad236e482c5b1fdc5d7bfc84381a26ddb164
KimSoonYo <rlatnsdy125@naver.com>
2022-10-12 03:01:23 +0900

    KSoonYo BAEKJOON 1106_호텔 solve

--- BAEKJOON/1106_호텔/s1_KSoonYo.py
+++ BAEKJOON/1106_호텔/s1_KSoonYo.py
@@ -2,23 +2,20 @@ import sys
 input = sys.stdin.readline
 
 C, N = map(int, input().split())
-dp = [0] * (1000 * 20 + 1)
-costs = {}
+dp = [float('inf')] * 100001
+dp[0] = 0
+promotions = {}
 for _ in range(N):
     cost, people = map(int, input().split())
-    costs[cost] = people
+    promotions[people] = min(promotions.get(people, 100), cost)
 
-last = 0
-for c in range(1, len(dp)):
+for p in range(1, len(dp)):
     possible = []
-    for p in costs.keys():
-        if p <= c:
-            possible.append(p)
+    for people in promotions.keys():
+        if people <= p:
+            possible.append(people)
+    for np in possible:
+        dp[p] = min(dp[p], dp[p - np] + promotions[np])
 
-    for nc in possible:
-        dp[c] = max(dp[c], dp[c - nc] + costs[nc])
-    
-    last = c
-    if dp[c] >= C:
-        break
-print(last)
+
+print(min(dp[C:]))
