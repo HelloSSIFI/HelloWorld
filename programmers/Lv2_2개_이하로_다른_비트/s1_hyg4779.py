f67a1ad004a885d4a975656057325f70168d1608
hong <hyg4779@naver.com>
2022-10-11 11:47:04 +0900

    programmers 100 옮기기

--- programmers/Lv2_2개_이하로_다른_비트/s1_hyg4779.py
+++ programmers/Lv2_2개_이하로_다른_비트/s1_hyg4779.py
@@ -1,10 +1,13 @@
 def solution(numbers):
     answer = []
     for num in numbers:
-
+        # 짝수인 경우 가장 마지막 비트가 무조건 0
+        # 따라서 1 더한 후 append
         if num%2==0:
             answer.append(num+1)
 
+        # 홀수의 경우 0이 나오는 가장 빠른 위치를 1로 바꾸고 그 다음 비트가 0이면
+        # 최대 2개만 치환하여 바꾼 가장 작은 수 완성
         else:
             word = '0'+bin(num)[2:]
             i = word.rfind('0')
