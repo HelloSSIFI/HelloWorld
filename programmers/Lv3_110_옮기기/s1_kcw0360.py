235c9518b4ad5bbbf6950dfe36053d5895f1b5fa
kcw0360 <kcw0360@naver.com>
2022-10-11 11:02:59 +0900

    Lv3 110 옮기기(수정)

--- programmers/Lv3_110_옮기기/s1_kcw0360.py
+++ programmers/Lv3_110_옮기기/s1_kcw0360.py
@@ -6,8 +6,9 @@ def solution(s):
         cnt = 0
         # 110 찾기
         for i in string:
-            if len(stack) >= 2 and i == '0' and stack[-2:] == ['1', '1']:
-                stack = stack[:-2]
+            if len(stack) >= 2 and i == '0' and stack[-2] == '1' and stack[-1] == '1':
+                stack.pop()
+                stack.pop()
                 cnt += 1
             else:
                 stack.append(i)
@@ -19,10 +20,8 @@ def solution(s):
                 break
             else:
                 check += 1
+
         # 카운트한 개수 앞에 까지의 stack + 110 개수 만큼 반복 + 나머지 1 모두 추가
         answer.append(''.join(stack[:len(stack)-check]) + '110'*cnt + '1'*check)
 
     return answer
-
-
-print(solution(	["1110", "100111100", "0111111010"]))
