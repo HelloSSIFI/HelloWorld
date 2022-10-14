f4098d1bda8b1fb9c29138184471bc9a3aa7bd82
kcw0360 <kcw0360@naver.com>
2022-10-14 16:39:50 +0900

    Lv2 할인 행사(수정)

--- programmers/Lv2_할인_행사/s1_kcw0360.py
+++ programmers/Lv2_할인_행사/s1_kcw0360.py
@@ -1,28 +1,26 @@
+# 2022 코딩테스트 실전 대비 모의고사 2번과 같은 문제(더 빠른 풀이)
 from collections import defaultdict
-import copy
 
 
 def solution(want, number, discount):
     answer = 0
     buy = defaultdict(int)
+    for idx, val in enumerate(want):
+        buy[val] = idx
 
-    for a, b in zip(want, number):    # 구입 품목: 개수 형태의 dict 생성
-        buy[a] = b
-
-    # 10개씩 잘라서 구입 품목 및 개수가 맞는지 확인
     for i in range(len(discount)-9):
-        check = copy.deepcopy(buy)    # 구입 품목과 개수가 적인 dict 복사
-        temp = discount[i:i+10]    # 10일 구간 나누기
+        temp = [0] * len(number)
         flag = True
 
-        for j in temp:
-            if check[j]:    # 구매 체크
-                check[j] -= 1
-            else:    # 구입 품목에 없는 품목이 있다면 반복문 중단하기
+        for j in range(i, i+10):
+            if discount[j] in buy:
+                temp[buy[discount[j]]] += 1
+            else:
                 flag = False
                 break
 
-        if flag:    # 모두 구입이 가능하다면 answer +1
-            answer += 1
+        if flag:
+            if temp == number:
+                answer += 1
 
     return answer
