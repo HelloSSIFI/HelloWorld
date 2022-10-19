772fe45f5b8444631fd058f81a451f229f16dea2
kcw0360 <kcw0360@naver.com>
2022-10-19 15:37:05 +0900

    1477 휴게소 세우기(주석 수정)

--- BAEKJOON/1477_휴게소_세우기/s1_kcw0360.py
+++ BAEKJOON/1477_휴게소_세우기/s1_kcw0360.py
@@ -3,19 +3,19 @@ input = sys.stdin.readline
 
 
 N, M, L = map(int, input().split())
-area = [0] + list(map(int, input().split())) + [L]    # 고속도로의 시작, 휴게소 위치, 고속도의 끝을 리스트로 생성
+area = [0] + list(map(int, input().split())) + [L]    # 고속도로의 시작, 휴게소 위치, 고속도로의 끝을 리스트로 생성
 area.sort()    # 휴게소 위치에 파악을 위한 정렬
 answer = 0
 start, end = 1, L
 while start <= end:
     mid = (start + end) // 2    # 예상 거리
 
-    cnt = 0
+    cnt = 0    # 휴게소 개수 체크
     # 현재 존재하는 휴게소 끼리 최대거리 찾기
     for i in range(1, len(area)):
         tmp = area[i] - area[i-1]    # 휴게소 사이의 거리
         if tmp > mid:    # mid보다 휴게소 사이의 거리가 큰 경우
-            cnt += (tmp-1)//mid    # 휴게소 설립 후 cnt 건설된 휴게소
+            cnt += (tmp-1)//mid    # 휴게소 지을 수 있는 개수를 cnt에 추가
 
     if cnt > M:    # 휴게소 개수가 예상보다 많은 경우
         start = mid + 1    # mid의 증가를 위해 start를 mid+1 값으로 대
