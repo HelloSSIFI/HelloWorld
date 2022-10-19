770c9ca0cb2a20307b8be75499054ec959a38753
hong <hyg4779@naver.com>
2022-10-19 16:47:59 +0900

    programmers lv2_혼자놀기의달인

--- programmers/Lv2_혼자_놀기의_달인/s1_hyg4779.py
+++ programmers/Lv2_혼자_놀기의_달인/s1_hyg4779.py
@@ -1,32 +1,38 @@
+from collections import deque
 def solution(cards):
+    '''
+    만들 수 있는 모든 그래프를 그리고,
+    그래프의 길이를 graphes에 담아서
+    모든 곱의 조합을 찾아서 최대값 리턴
+    '''
     answer = 0
-    cards = [0] + cards
+    cards = [0]+cards
+
     n = len(cards)
+    graphes = []
+    visit = [0]*(n+1)
 
-    one_visit = [0]*n
+    for i in range(1,n):
+        if visit[i] == 0:
 
-    for idx in range(1, n):
-        if one_visit[idx] == 0:
-            one = []
-            while True:
-                if one_visit[idx] == 0:
-                    one += [cards[idx]]
-                    one_visit[idx] = 1
-                    idx = cards[idx]
-                else:
-                    for jdx in range(2, n):
-                        if one_visit[jdx] == 0:
-                            two = []
-                            while True:
-                                if one_visit[jdx] == 0:
-                                    two += [cards[jdx]]
-                                    one_visit[jdx] = 1
-                                    jdx = cards[jdx]
-                                else:
-                                    answer = max(answer, len(one)*len(two))
-                                    break
-                    break
+            Q = deque([cards[i]])
+            cnt = 0
+            while Q:
+                now = Q.popleft()
+                cnt += 1
+
+                visit[now] = 1
+                next = cards[now]
+                if visit[next] == 0:
+                    Q.append(next)
+
+            graphes.append(cnt)
+
+    for i in range(len(graphes)-1):
+        for j in range(i+1, len(graphes)):
+            answer = max(answer, graphes[i]*graphes[j])
 
     return answer
+
 
 print(solution([8, 6, 3, 7, 2, 5, 1, 4]))
