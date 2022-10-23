a66c404aa7dc7a5118feaf6a1d39743d13b60f0f
KimSoonYo <rlatnsdy125@naver.com>
2022-10-23 19:02:11 +0900

    KSoonYo BAEKJOON 15927_회문은_회문아니야 풀이 수정

--- BAEKJOON/15927_회문은_회문아니야/s1_KSoonYo.py
+++ BAEKJOON/15927_회문은_회문아니야/s1_KSoonYo.py
@@ -1,35 +1,22 @@
-
 S = input()
 L = len(S)
 
 
-# 문자열 전체가 회문이 아닌 경우
-# 문자열 하나도 팰런드롬
-isPalind = True
+# 문자열 전체가 같은 문자로만 이루어져있는 경우
+if len(set(S)) == 1:
+    print(-1)
+    exit()
 
+# 문자열 전체가 회문이 아닌 경우
 p1 = 0
 p2 = L - 1
-while p1 < p2:
+while p1 <= p2:
     if S[p1] != S[p2]:
-        isPalind = False
         print(L)
-        break
+        exit()
     p1 += 1
     p2 -= 1
 
 # 문자열 전체가 회문인 경우
 # 앞이나 뒤에서 문자 하나를 뺌
-# 문자를 뺐는데도 회문이면 모든 경우에 대해서 팰런드롬
-p1 = 0
-p2 = L - 2
-while isPalind and p1 < p2:
-    if S[p1] != S[p2]:
-        isPalind = False
-        print(L - 1)
-        break
-    p1 += 1
-    p2 -= 1
-
-if isPalind:
-    print(-1)
-
+print(L - 1)
