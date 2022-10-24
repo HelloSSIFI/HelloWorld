9797b995643f3795afce02c482782f6aeb95385b
sangbaek <tlsqktem483@naver.com>
2022-10-24 14:38:38 +0900

    Solve: BAEKJOON 2624

---
+++ BAEKJOON/2624_동전_바꿔주기/s1_tlsqktem483.py
@@ -0,0 +1,19 @@
+"""
+PyPy 통과, Python3 시간초과
+"""
+T = int(input())
+k = int(input())
+coin = []
+dp = [0] * (T+1)
+dp[0] = 1
+
+for _ in range(k):
+    c, n = map(int, input().split())
+    coin.append([c, n])
+
+for c_val, cnt in coin:
+    for m in range(T, 0, -1):
+        for c in range(1, cnt+1):
+            if m - c_val*c >= 0:
+                dp[m] += dp[m - c_val*c]
+print(dp[T])
