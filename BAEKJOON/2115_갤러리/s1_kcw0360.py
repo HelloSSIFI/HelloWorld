6773f0428ddb9f97f78d3cbc3ec03641e04a97df
kcw0360 <kcw0360@naver.com>
2022-10-29 17:54:56 +0900

    2115 갤러리(주석 수정)

--- BAEKJOON/2115_갤러리/s1_kcw0360.py
+++ BAEKJOON/2115_갤러리/s1_kcw0360.py
@@ -4,27 +4,27 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())    # N: 세로, M: 가로
 arr = [list(input()) for _ in range(N)]
-check = [[[0]*M for _ in range(N)] for _ in range(4)]    # 그림 걸기 [0: 좌, 1: 우, 2: 상, 3: 하]
+check = [[[0]*M for _ in range(N)] for _ in range(4)]    # 그림 걸는 곳 체크 [0: 좌, 1: 우, 2: 상, 3: 하][y][x]
 answer = 0
 
 for y in range(1, N-1):
     for x in range(1, M-1):
         if arr[y][x] == '.':    # 현 위치가 빈 공간인 경우
-            if arr[y+1][x] == ".":    # 현 위치의 윗칸도 빈 공간인 경우
-                if arr[y][x-1] == "X" and arr[y+1][x-1] == "X" and check[0][y][x] == 0:    # 빈 공간 좌측이 벽이면서 그림이 안걸려있다면
+            if arr[y+1][x] == '.':    # 현 위치의 윗칸도 빈 공간인 경우
+                if arr[y][x-1] == 'X' and arr[y+1][x-1] == 'X' and check[0][y][x] == 0:    # 빈 공간 좌측이 벽이면서 그림이 안걸려있다면
                     check[0][y][x] = 1    # 그림 걸기
                     check[0][y+1][x] = 1    # 그림 걸기
                     answer += 1    # 그림 개수 추가
-                if arr[y][x + 1] == "X" and arr[y+1][x+1] == "X" and check[1][y][x] == 0:
+                if arr[y][x + 1] == 'X' and arr[y+1][x+1] == 'X' and check[1][y][x] == 0:    # 빈 공간 우측이 벽
                     check[1][y][x] = 1
                     check[1][y+1][x] = 1
                     answer += 1
-            if arr[y][x+1] == ".":    # 현 위치의 우측도 빈 공간인 경우
-                if arr[y-1][x] == "X" and arr[y-1][x+1] == "X" and check[2][y][x] == 0:
+            if arr[y][x+1] == '.':    # 현 위치의 우측도 빈 공간인 경우
+                if arr[y-1][x] == 'X' and arr[y-1][x+1] == 'X' and check[2][y][x] == 0:    # 빈 공간 위가 벽
                     check[2][y][x] = 1
                     check[2][y][x+1] = 1
                     answer += 1
-                if arr[y+1][x] == "X" and arr[y+1][x+1] == "X" and check[3][y][x] == 0:
+                if arr[y+1][x] == 'X' and arr[y+1][x+1] == 'X' and check[3][y][x] == 0:    # 빈 공간 아래가 벽
                     check[3][y][x] = 1
                     check[3][y][x+1] = 1
                     answer += 1
