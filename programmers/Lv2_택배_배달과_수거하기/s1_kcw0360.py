2cd8374c553f09255f013f31c75ded52354f7769
kcw0360 <kcw0360@naver.com>
2023-02-11 09:48:33 +0900

    주석 추가

--- programmers/Lv2_택배_배달과_수거하기/s1_kcw0360.py
+++ programmers/Lv2_택배_배달과_수거하기/s1_kcw0360.py
@@ -1,20 +1,20 @@
 def solution(cap, n, deliveries, pickups):
     answer = 0
 
-    while deliveries and deliveries[-1] == 0:
+    while deliveries and deliveries[-1] == 0:    # 가장 먼 집부터 차례로 체크하며 배달해야할 첫 집 찾기
         deliveries.pop()
 
-    while pickups and pickups[-1] == 0:
+    while pickups and pickups[-1] == 0:    # 가장 먼 집부터 차례로 체크하며 수거해야할 첫 집 찾기
         pickups.pop()
 
     while deliveries or pickups:
-        answer += max(len(deliveries), len(pickups)) * 2
+        answer += max(len(deliveries), len(pickups)) * 2    # 둘 중 먼 곳 집의 거리를 answer에 추가(왕복거리라 *2)
 
-        box = 0
+        box = 0    # 현재 박스 수
         while box <= cap and deliveries:
-            if deliveries[-1] + box <= cap:
+            if deliveries[-1] + box <= cap:    # 트럭에 실을 수 있는 최대치 보다 적은 경우
                 box += deliveries.pop()
-            else:
+            else:    # 최대치 보다 많은 경우
                 deliveries[-1] -= cap-box
                 break
 
--- programmers/Lv3_표_병합/s1_kcw0360.py
+++ programmers/Lv3_표_병합/s1_kcw0360.py
@@ -35,37 +35,39 @@ def solution(commands):
         command = command.split()
 
         if command[0] == 'UPDATE':
-            if len(command) == 3:
+            if len(command) == 3:    # val1을 val2로 변환
                 for i in range(1, 51):
                     for j in range(1, 51):
                         y, x = find(i, j)
                         if graph[y][x] == command[1]:
                             graph[y][x] = command[2]
-            else:
+            else:    # 해당 위치의 셀을 val로 변환
                 y, x = find(int(command[1]), int(command[2]))
                 graph[y][x] = command[3]
-        elif command[0] == 'MERGE':
+
+        elif command[0] == 'MERGE':    # 병합
             union(int(command[1]), int(command[2]), int(command[3]), int(command[4]))
-        elif command[0] == 'UNMERGE':
+
+        elif command[0] == 'UNMERGE':    # 병합 해제
             y, x = find(int(command[1]), int(command[2]))
             before = graph[y][x]
-            temp = []
+            temp = []    # 병합 해제하기 위한 좌표 저장
 
-            for i in range(1, 51):
+            for i in range(1, 51):    # 병합 해제하기 위한 좌표 찾기
                 for j in range(1, 51):
                     if i == command[1] and j == command[2]:
                         continue
                     if [y, x] == find(i, j):
                         temp.append([i, j])
 
-            for a, b in temp:
+            for a, b in temp:    # 병합 해제
                 parent[a][b] = [a, b]
                 graph[a][b] = 'EMPTY'
 
             parent[int(command[1])][int(command[2])] = [int(command[1]), int(command[2])]
             graph[int(command[1])][int(command[2])] = before
 
-        else:
+        else:    # 프린트
             y, x = find(int(command[1]), int(command[2]))
             answer.append(graph[y][x])
 
